ed609adced6bc2cd76fd7d74e5ae5a0e440792e5
Vasiliy Aboymov <aboymov@gmail.com>
2021-12-07 09:57:38 +0300

    tabs->spaces

--- blocks.def.h
+++ blocks.def.h
@@ -1,12 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/		/*Command*/												/*Update Interval*/	/*Update Signal*/
-	{" ",			"xkb-switch",											0,					1},
-	{"",			"curl https://wttr.in/?format='%c:+%t'",				600,				0},
-	{"Updates: ",	"checkupdates | wc -l",									600,				0},
-	{"USD: ", 		"printf \"%.2f\n\" $(curl -s rub.rate.sx/1USD | bc)",	1800,				0},
-	{"CPU: ",		"sensors | sed -e 's/Tdie: *\\(.*\\)  .*/\\1/;t;d'",	10,					0},
-	{"",			"date '+%d.%m.%Y %H:%M'",								60,					0},
+    /*Icon*/        /*Command*/                                                /*Update Interval*/    /*Update Signal*/
+    {" ",            "xkb-switch",                                             0,                     1},
+    {"",             "curl https://wttr.in/?format='%c:+%t'",                  600,                   0},
+    {"Updates: ",    "checkupdates | wc -l",                                   600,                   0},
+    {"USD: ",        "printf \"%.2f\n\" $(curl -s rub.rate.sx/1USD | bc)",     1800,                  0},
+    {"CPU: ",        "sensors | sed -e 's/Tdie: *\\(.*\\)  .*/\\1/;t;d'",      10,                    0},
+    {"",             "date '+%d.%m.%Y %H:%M'",                                 60,                    0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
