78b2a81293f1580c25ad77e7896046dc61719af1
stee1rat <aboymov@gmail.com>
2022-07-07 18:16:28 +0300

    added battery status

--- blocks.def.h
+++ blocks.def.h
@@ -1,17 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/        /*Command*/                                                /*Update Interval*/    /*Update Signal*/
-	//{" ",           "l=$(xkb-switch)&&echo ${l^^} ",                                              0,                     1},
-	{" ",           "xkb-switch",                                              0,                     1},
-	//{"",            "curl https://wttr.in/?format='%c%t'|grep -v Unknown",    600,                   0},
-	{"",            "curl http://wttr.in/Moscow?format='%x%t'|grep -v Unknown",    600,                   0},
-	{"u:",            "checkupdates | wc -l",                    600,                   0},
-	{"d:",   "printf \"%.2f\n\" $(curl -s rub.rate.sx/1USD | bc)",      1800,                  0},
-	{"t:",       "sensors | sed -e 's/Tdie: *\\(.*\\)  .*/\\1/;t;d'",       10,                    0},
-	{"",            "date '+%d.%m.%Y %H:%M'",                                  60,                    0},
+	/*Icon*/	/*Command*/							/*Update Interval*/    /*Update Signal*/
+	{" ",		"xkb-switch",							0,                     1},
+	{"",		"curl http://wttr.in/Moscow?format='%x%t'|grep -v Unknown",	600,                   0},
+	{"u:",		"checkupdates | wc -l",						600,                   0},
+	{"d:",		"printf \"%.2f\n\" $(curl -s rub.rate.sx/1USD | bc)",		1800,                  0},
+	{"t:", 		"sensors | sed -e 's/temp1: *\\(.*\\)  .*/\\1/;t;d'",		10,                    0},
+	{"b:", 		"scripts/battery.sh",						10,                    0},
+	{"",		"date '+%d.%m.%Y %H:%M'",					60,                    0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-//static char delim[] = " ";
 static char delim[] = " ";
 static unsigned int delimLen = 5;
